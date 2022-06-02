1050d9a0f642775fc30796c7ea1b5dc2273710ac
michstery <macaulaymich@gmail.com>
2022-06-02 12:09:50 +0100

    tokenization of reset password

--- models/userModel.js
+++ models/userModel.js
@@ -85,16 +85,19 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
   };
 
   userSchema.methods.createPasswordResetToken = function() {
-      const resetToken = crypto.randomBytes(32).toString('hex');
-
-      this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
-
-      console.log({ resetToken }, this.passwordResetToken)
-
-      this.passwordResetExpires = Date.now() +10 *60 *1000;
-
-      return resetToken;
-  }
+    const resetToken = crypto.randomBytes(32).toString('hex');
+  
+    this.passwordResetToken = crypto
+      .createHash('sha256')
+      .update(resetToken)
+      .digest('hex');
+  
+    console.log({ resetToken }, this.passwordResetToken);
+  
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+  
+    return resetToken;
+  };
 
 const User = mongoose.model('User', userSchema);
 
