891f07c2df2a4522664ecabcb3a3da372b01e56e
michstery <macaulaymich@gmail.com>
2022-06-13 09:46:51 +0100

    finished with the user update functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const User = require('../models/userModel');
 const AppError = require('../utilities/appError');
 const catchAsync = require('../utilities/catchAsync');
 
+
 /////////\\\\\\\\\  ~ USERS HANDLER FUNCTIONS ~ ////////////////\\\\\\\\\\\
 exports.getAllUsers = catchAsync(async(req, res) => {
     const users = await User.find();
@@ -14,17 +15,27 @@ exports.getAllUsers = catchAsync(async(req, res) => {
     });
 });
 
-exports.updateMe = (req, res, next) => {
+exports.updateMe = catchAsync( async (req, res, next) => {
     // 1) create error if user POSTs password data
     if (req.body.password || req.body.passwordConfirm) {
         return next(new AppError("This route is not for password updates. Use /updatePassword route", 400));
     }
 
     //2) update user document
+    const { name, email } = req.body;
+    const filteredBody = {name,email} 
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+        new: true,
+        runValidators: true
+    });
+
     res.status(200).json({
-        status: 'success'
+        status: 'success',
+        data: {
+            user: updatedUser
+        }
     })
-}
+})
 
 exports.createNewUser = (req, res) => {
     res.status(500).json({
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -10,6 +10,7 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword); 
 router.patch('/resetPassword/:token', authController.resetPassword); 
 router.patch('/updatePassword', authController.protect, authController.updatePassword); 
+router.patch('/updateMe', authController.protect, userController.updateMe);
 
 
 /////////\\\\\\\\\  ~ USERS ROUTES ~ ////////////////\\\\\\\\\\\
