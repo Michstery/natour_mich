5660482df9522fd098b94de245d578bd9584fec6
michstery <macaulaymich@gmail.com>
2022-06-16 22:33:45 +0100

    started working on sending JWT using cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,13 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
+    const cookieOptions =  {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 6 * 6 * 1000),
+        httpOnly: true
+    }
+    if(process.env.NODE_ENV == 'production') cookieOptions.secure == true;
+
+    res.cookie('jwt', token, cookieOptions);
 
     res.status(statusCode).json({
         status: 'success',
