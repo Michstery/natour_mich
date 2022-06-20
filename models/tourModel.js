6278d18e5f343ce14045ef1cef599ff1b591e0c7
michstery <macaulaymich@gmail.com>
2022-06-20 13:25:45 +0100

    tried implementing embedded type modelling for guides inside Tour dataset

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('./userModel')
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -103,10 +104,12 @@ const tourSchema = new mongoose.Schema({
                 },
                 coordinates: [Number],
                 address: String,
-                description: String
+                description: String,
+                day: Number
         }
         }
-    ]
+    ],
+    guides: Array
 },
     {
         toJSON: {virtuals: true},
@@ -120,11 +123,17 @@ tourSchema.virtual('durationWeeks').get(function(){
     return this.duration /7;
 });
 // DOCUMENT MIDDLEWARE: runs before .save and .create() .
-// tourSchema.pre('save', function(next){
+tourSchema.pre('save', function(next){
   
-//     this.slug = slugify(this.name, {lower: true});
-//     next();
-// });
+    this.slug = slugify(this.name, {lower: true});
+    next();
+});
+
+tourSchema.pre('save', async function(next){
+    const guidesPromises = this.guides.map(async id  => await User.findById(id));
+    this.guides = await Promise.all(guidesPromises);
+    next()
+})
 
 //DOCUMENT MIDDLEWARE: POST Hook mongoose middleware
 // tourSchema.post('save', function(doc, next){
