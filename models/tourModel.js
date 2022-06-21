a816e7a45f952e3755c183117cedc379879b56b6
michstery <macaulaymich@gmail.com>
2022-06-21 10:02:53 +0100

    used child style referencing for guides inside tour model (test)

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
-const User = require('./userModel')
+// const User = require('./userModel')
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -109,7 +109,12 @@ const tourSchema = new mongoose.Schema({
         }
         }
     ],
-    guides: Array
+    guides: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User'
+        }
+    ]
 },
     {
         toJSON: {virtuals: true},
@@ -129,11 +134,11 @@ tourSchema.pre('save', function(next){
     next();
 });
 
-tourSchema.pre('save', async function(next){
-    const guidesPromises = this.guides.map(async id  => await User.findById(id));
-    this.guides = await Promise.all(guidesPromises);
-    next()
-})
+// tourSchema.pre('save', async function(next){
+//     const guidesPromises = this.guides.map(async id  => await User.findById(id));
+//     this.guides = await Promise.all(guidesPromises);
+//     next()
+// })
 
 //DOCUMENT MIDDLEWARE: POST Hook mongoose middleware
 // tourSchema.post('save', function(doc, next){
