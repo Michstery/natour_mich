d5b71b614fd5f4a7d2261e3d272039f3677102de
michstery <macaulaymich@gmail.com>
2022-06-22 13:00:27 +0100

    created the review controllers files

---
+++ controllers/reviewController.js
@@ -0,0 +1,27 @@
+const Review = require('../models/reviewModel');
+const catchAsync = require('../utilities/catchAsync');
+const AppError = require('../utilities/appError');
+
+exports.getAllReviews = catchAsync( async (req, res, next) => {
+    const reviews = await Review.find();
+    res.status(200).json({
+        status: 'success',
+        data: {
+            reviews
+        }
+    })
+});
+
+exports.createReview = catchAsync(async (req, res, neext) =>{
+    const newReview = await Review.create(req.body);
+    if(!newReview){
+        return next(new AppError('No Review Created', 400))
+     }
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            newReview
+        }
+    })
+});
