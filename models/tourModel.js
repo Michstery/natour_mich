daeb30b020fcfae69d2a37a679bb10c2de5808e2
michstery <macaulaymich@gmail.com>
2022-06-24 19:09:20 +0100

    started to virtual populate tour model with reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -114,7 +114,11 @@ const tourSchema = new mongoose.Schema({
             type: mongoose.Schema.ObjectId,
             ref: 'User'
         }
-    ]
+    ],
+    review: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Review'
+    }
 },
     {
         toJSON: {virtuals: true},
@@ -127,6 +131,12 @@ const tourSchema = new mongoose.Schema({
 tourSchema.virtual('durationWeeks').get(function(){
     return this.duration /7;
 });
+tourSchema.virtual('reviews', {
+    ref: 'Review',
+    foreignField: 'tour',
+    localField: '_id'
+})
+
 // DOCUMENT MIDDLEWARE: runs before .save and .create() .
 tourSchema.pre('save', function(next){
   
