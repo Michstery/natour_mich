0111d04ca333427f3b860d8565e89f1ac98521d2
michstery <macaulaymich@gmail.com>
2022-07-08 11:25:42 +0100

    finished with the get tours distance implementation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -141,7 +141,7 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
     const { distance, latlng, unit } = req.params;
     const [lat, lng] = latlng.split(',');
 
-    const radius = unit === 'miles' ? distance / 3963.2 : distance / 6378.1;
+    const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
 
     if(!lat || !lng) {
         next(new AppError('please provide latitude and longitude in the format lat, lng.', 400))
@@ -163,18 +163,28 @@ exports.getDistance = catchAsync(async (req, res, next) => {
     const { latlng, unit } = req.params;
     const [lat, lng] = latlng.split(',');
 
+    const multiplier = unit === 'mi' ? 0.000621371 : 0.001 ;
+
     if(!lat || !lng) {
         next(new AppError('please provide latitude and longitude in the format lat, lng.', 400))
     };
 
-    const distances = Tour.aggregate([
+    const distances = await Tour.aggregate([
         {
             $geoNear: {
                 near: {
                     type: 'Point',
                     coordinates: [lng * 1, lat * 1]
                 },
-                distanceField: 'distance'
+                distanceField: 'distance',
+                distanceMultiplier: multiplier
+            },
+            
+        },
+        {
+            $project: {
+                distance: 1,
+                name: 1
             }
         }
     ]);
