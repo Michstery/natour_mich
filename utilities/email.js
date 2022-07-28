3c13429460bc467c8b1d4fbe52437e2b937fb179
michstery <macaulaymich@gmail.com>
2022-07-28 21:28:22 +0100

    building a class based email handler

--- utilities/email.js
+++ utilities/email.js
@@ -1,28 +1,61 @@
 const nodemailer = require('nodemailer');
 
-const sendEmail = async options => {
-  // 1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure:true,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+module.exports = class Email {
+  constructor(user, url){
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = user.url;
+    this.from = `Mich <${process.env.EMAIL_FROM}>`;
+  }
+
+  // 1) create Transporter
+  createTransport() {
+    if(process.env.NODE_ENV === 'production') {
+      // sendgrid
+      return 1;
     }
-  });
+   
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure:true,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
+
+  // 2) send the actual Email
+  send(template, subject) {
+    // i) render HTML Based on template = pug 
+
+    // ii) define email options
+    const mailOptions = {
+      from: 'Mich <mich@michstery.io>',
+      to: options.email,
+      subject: options.subject,
+      text: options.message
+      // html:
+    };
+
+    // iii) create a transport and send email
+
+  }
+
+  sendWelcome() {
+    this.send('welcome', 'Welcome To The MichNatours Family!')
+  }
+
+};
+
+const sendEmail = async options => {
+
 
   // 2) Define the email options
-  const mailOptions = {
-    from: 'Mich <mich@jonas.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-    // html:
-  };
+  
 
   // 3) Actually send the email
   await transporter.sendMail(mailOptions);
 };
 
-module.exports = sendEmail;
